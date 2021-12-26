e3c3fc5e41d6264fee5a28559906121799d7a430
Hibo Ali <hiba.mumin22@gmail.com>
2021-12-25 21:55:38 -0600

    defined form elements in js

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function (){
+
+    //define form elements in my form HTML
+    var form = document.getElementById("my-form");
+    
+    var status = document.getElementById("status");
+});

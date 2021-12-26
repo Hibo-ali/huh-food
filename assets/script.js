5fbf96b87da7e8ef7c73810bc54a325413dd1514
Hibo Ali <hiba.mumin22@gmail.com>
2021-12-25 22:02:40 -0600

    added succes and error function

--- assets/script.js
+++ assets/script.js
@@ -4,4 +4,14 @@ window.addEventListener("DOMContentLoaded", function (){
     var form = document.getElementById("my-form");
     
     var status = document.getElementById("status");
+
+    //Success and error function for after the form is submitted
+
+    function success() {
+        form.reset();
+        status.innerHTML = "Thanks for your submission!";
+    }
+    function error() {
+        status.innerHTML = "Oops! There was a problem submitting your form";
+    }
 });

1419b311742f0a96c8f66ad0b1bffb239d21db34
BurnsD <Dustinburnsactech@gmail.com>
2022-01-03 15:28:16 -0600

    new hamburger with drop down

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
+function openMenu() {
+    document.getElementById("myMenu").style.height = "100%"
+}
+
+function closeMenu() {
+    document.getElementById("myMenu").style.height = "0%"
+}
+

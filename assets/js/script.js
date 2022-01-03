e80fd1efd1b54c86c46768932b5ad399004e0178
Hibo Ali <hiba.mumin22@gmail.com>
2022-01-03 17:26:03 -0600

    commented out js for the moment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,24 @@
-var slideshowImages = document.querySelectorAll(".slideshow-container .imgs");
+// var slideshowImages = document.querySelectorAll(".slideshow-container .imgs");
 
-var nextImgDelay = 5000;
-let currentImg = 0;
+// var nextImgDelay = 5000;
+// let currentImg = 0;
 
-slideshowImages[currentImg].style.display = "block";
+// // slideshowImages[currentImg].style.display = "block";
+// slideshowImages[currentImg].style.opacity = 1;
 
-setInterval(nextImg, nextImgDelay);
+// setInterval(nextImg, nextImgDelay);
 
-function nextImg() {
-    slideshowImages[currentImg].style.display = "none";
-    currentImg = (currentImg + 1) % slideshowImages.length;
-    slideshowImages[currentImg].style.display = "block";
+// function nextImg() {
+//     // slideshowImages[currentImg].style.display = "none";
+//     // slideshowImages[currentImg].style.opacity = 0;
 
-}
+    
+//     setTimeout(() => {
+//         slideshowImages[].style.opacity = 0;
+//     }, 1000);
+
+//     currentImg = (currentImg + 1) % slideshowImages.length;
+//     // slideshowImages[currentImg].style.display = "block";
+//     slideshowImages[currentImg].style.opacity = 1;
+
+// }

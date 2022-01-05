9bcd94ce34cd27474d560bcdc1aad4b9c01aaffe
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-01-04 19:59:20 -0600

    stylesheet

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+window.addEventListener("DOMContentLoaded", function (){
+
+    //define form elements in my form HTML
+    var form = document.getElementById("my-form");
+    
+    var status = document.getElementById("status");
+
+    //Success and error function for after the form is submitted
+
+    function success() {
+        form.reset();
+        status.classList.add('success');
+        status.innerHTML = "Thanks for your submission!";
+    }
+    function error() {
+        status.classList.add("error");
+        status.innerHTML = "Oops! There was a problem submitting your form";
+    }
+
+    // handle the form submission event
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
+});
+

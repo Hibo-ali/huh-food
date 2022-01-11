a6f14f26a2b0f3d4fed5348f0ce4bd3f608590a2
Nehailaa <93628865+Nehailaa@users.noreply.github.com>
2022-01-10 20:42:23 -0600

    jquery api with for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,105 +1,42 @@
-// window.addEventListener("DOMContentLoaded", function () {
 
-//     //define form elements in my form HTML
-//     var form = document.getElementById("my-form");
-
-//     var status = document.getElementById("status");
-
-//     //Success and error function for after the form is submitted
-
-//     function success() {
-//         form.reset();
-//         status.classList.add('success');
-//         status.innerHTML = "Thanks for your submission!";
-//     }
-//     function error() {
-//         status.classList.add("error");
-//         status.innerHTML = "Oops! There was a problem submitting your form";
-//     }
-
-//     // handle the form submission event
-    
-//     form.addEventListener("submit", function (ev) {
-//         ev.preventDefault();
-//         var data = new FormData(form);
-//         ajax(form.method, form.action, data, success, error);
-//     });
-// });
-
-// // helper function for sending an AJAX request
-// function ajax(method, url, data, success, error) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open(method, url);
-//     xhr.setRequestHeader("Accept", "application.json");
-//     xhr.onreadystatechange = function () {
-//         if (xhr.readyState !== XMLHttpRequest.DONE) return;
-//         if (xhr.status === 200) {
-//             success(xhr.response, xhr.responseType);
-//         } else {
-//             (xhr.status, xhr.response, xhr.responseType);
-//         }
-//     };
-//     xhr.send(data);
-
-// }
-// Mobile Menu
-
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
-
-burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active')
-});
+	
 // Worldwide Restaurants API Documentation
 
 //Using jQuery
-// const settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://worldwide-restaurants.p.rapidapi.com/search",
-// 	"method": "POST",
-// 	"headers": {
-// 		"content-type": "application/x-www-form-urlencoded",
-// 		"x-rapidapi-key": "8a4b8f4af6mshad697da7b4112c7p19d468jsnf30df0617daa",
-// 		"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com"
-// 	},
-// 	"data": {
-// 		"language": "en_US",
-// 		"limit": "30",
-// 		"location_id": "297704",
-// 		"currency": "USD"
-// 	}
-// };
 
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
-
-function getResults(restName) {
-
-    return fetch("https://worldwide-restaurants.p.rapidapi.com/search?id=${restName}", {
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://worldwide-restaurants.p.rapidapi.com/search",
 	"method": "POST",
 	"headers": {
 		"content-type": "application/x-www-form-urlencoded",
 		"x-rapidapi-host": "worldwide-restaurants.p.rapidapi.com",
-		"x-rapidapi-key": "8a4b8f4af6mshad697da7b4112c7p19d468jsnf30df0617daa"
+		"x-rapidapi-key": "556ea67667mshcf82cd7d109853fp12f94djsnd82e5f1d23ec"
 	},
-	"body": {
+	"data": {
 		"language": "en_US",
 		"limit": "30",
 		"location_id": "297704",
 		"currency": "USD"
 	}
-})
-.then(response => {
-    return response.json().then((data) => {
-	console.log(data);
-    return data;
-})
-})
-.catch(err => {
-	console.error(err);
-});
-}
+};
 
-getResults();
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
+
+var searchBtn = document.querySelector(".search-btn");
+
+searchBtn.addEventListener("click", function(){
+	$.ajax(settings).done(function (response) {
+		var dataInfo = response.results.data;
+		for ( var i = 0; i < dataInfo.length; i++){
+			var info = document.createElement("a");
+			info.textContent = dataInfo[i].write_review;
+			document.querySelector(".test").appendChild(info);
+		}
+		console.log(response);
+	});
+});
+
